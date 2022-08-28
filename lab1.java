a42abc9b459f3f037bd1689e7bf7dc4c8ab23bb9
GregoryPetterson <89867803+GregoryPetterson@users.noreply.github.com>
2022-08-28 14:39:38 -0500

    Add files via upload

---
+++ lab1.java
@@ -0,0 +1,46 @@
+//input: n = 3
+//output: 1 2 3
+//1 3 2
+//2 1 3 
+//2 3 1
+//3 1 2 
+//3 2 1
+
+public class Lab1 {
+   
+    public static void printArray(int[] a) 
+    {
+        for(int i=0; i<a.length; i++)
+        System.out.print(a[i] + " ");
+    }
+    
+    public static void swap(int[]a, int i, int j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    
+    public static void printPermutations(int[] a, int cid)
+    {
+        if(cid == a.length-1)
+        {
+            printArray(a);
+            return;
+        }
+
+        for(int i=cid; i<a.length; i++)
+        {
+            swap(a, i, cid);
+            printPermutations(a, cid+1);
+            swap(a, i, cid);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3};
+        printPermutations(a, 0);
+    }
+
+}
